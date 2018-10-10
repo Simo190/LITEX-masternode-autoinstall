d78cdb874538568a3ccef999e92d4982dd1ad1fd
Simone <37718696+Simo190@users.noreply.github.com>
2018-10-10 18:08:29 +0200

    Update Upload ltx-control.sh

--- Upload ltx-control.sh
+++ Upload ltx-control.sh
@@ -1,9 +1,8 @@
   #!/bin/bash
   
   function prepare_system() {
-cd
+
 rm ltx-control.sh
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
 wget https://raw.githubusercontent.com/Simo190/LITEX-masternode-autoinstall/master/ltx-control.sh && chmod +x ltx-control.sh
 clear
 }
