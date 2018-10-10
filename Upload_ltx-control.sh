0668ea908f56ace36c8e705ee87e2c8ec699ec23
Simone <37718696+Simo190@users.noreply.github.com>
2018-10-10 18:24:22 +0200

    Delete Upload_ltx-control.sh

--- Upload_ltx-control.sh
+++
@@ -1,9 +0,0 @@
-  #!/bin/bash
-  
-  function prepare_system() {
-
-rm ltx-control.sh
-wget https://raw.githubusercontent.com/Simo190/LITEX-masternode-autoinstall/master/ltx-control.sh && chmod +x ltx-control.sh
-clear
-exit 1
-}
